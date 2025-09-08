85b7ca74f1e31042e5cc4b7314836653ff86ae9b
John O'Sullivan <osullivj@gmail.com>
2025-09-08 13:30:00 +0100

    add ND_BOOST_HOME

--- sh/ndroot.bat
+++ sh/ndroot.bat
@@ -4,3 +4,4 @@ set "ND_MSVC_HOME=C:\Program Files\Microsoft Visual Studio\2022\Community"
 set ND_PY_HOME=C:\osullivj\bin\py3.12.3x64
 set ND_WABT_HOME=C:\osullivj\bld\wabt\bin
 set ND_GNU_HOME=c:\osullivj\bin
+set ND_BOOST_HOME=c:\osullivj\bld\boost_1_79_0
