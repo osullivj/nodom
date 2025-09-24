c472469b3da72ff40469f99dbd4f638102805ccd
John O'Sullivan <osullivj@gmail.com>
2025-09-24 16:08:31 +0100

    add ND_SSL_HOME for FireDaemon SSL binaries

--- sh/ndroot.bat
+++ sh/ndroot.bat
@@ -5,3 +5,4 @@ set ND_PY_HOME=C:\osullivj\bin\py3.12.3x64
 set ND_WABT_HOME=C:\osullivj\bld\wabt\bin
 set ND_GNU_HOME=c:\osullivj\bin
 set ND_BOOST_HOME=c:\osullivj\bld\boost_1_79_0
+set ND_SSL_HOME=c:\osullivj\bld\openssl-3.5.3
