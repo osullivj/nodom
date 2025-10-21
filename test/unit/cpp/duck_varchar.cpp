ee7954581e164e7f91118749a7a9e6efccfaf9b4
John O'Sullivan <osullivj@gmail.com>
2025-10-21 16:58:01 +0100

    both tests passing with real value checks

--- test/unit/cpp/duck_varchar.cpp
+++ test/unit/cpp/duck_varchar.cpp
@@ -6,7 +6,7 @@
 struct DuckDBFixture {
     duckdb_database db;
     duckdb_connection con;
-    std::map<idx_t, std::vector<std::string>> varchars;
+    std::map<idx_t, std::vector<std::string>> values;
 
     DuckDBFixture() {
         auto ret_val = duckdb_open(NULL, &db);
@@ -42,6 +42,7 @@ struct DuckDBFixture {
         double  decimal_value = 0.0;
 
         char buf[32];
+        char fmtbuf[16];
 
         for (idx_t row = 0; row < row_count; row++) {
             for (idx_t col = 0; col < colm_count; col++) {
@@ -49,6 +50,9 @@ struct DuckDBFixture {
                 colm_validity = duckdb_vector_get_validity(colm);
                 duckdb_logical_type type_l = duckdb_vector_get_column_type(colm);
                 duckdb_type type = duckdb_get_type_id(type_l);
+                // yes, this fires the default ctor for std::vector<std::string>
+                // on 1st visit to a column
+                std::vector<std::string>& strvec(values[col]);
                 if (!duckdb_validity_row_is_valid(colm_validity, row)) {
                     printf("NULL\t");
                     continue;
@@ -64,8 +68,9 @@ struct DuckDBFixture {
                     break;
                 case DUCKDB_TYPE_INTEGER:   // int32_t
                     idata = (int32_t*)duckdb_vector_get_data(colm);
-                    sprintf(buf, "%I32d\t", idata[row]);
+                    sprintf(buf, "%I32d", idata[row]);
                     printf(buf);
+                    strvec.push_back(buf);
                     break;
                 case DUCKDB_TYPE_UTINYINT:  // uint8_t
                     BOOST_FAIL("DUCKDB_TYPE_UTINYINT unimplemented");
@@ -81,13 +86,15 @@ struct DuckDBFixture {
                     break;
                 case DUCKDB_TYPE_BIGINT:    // int64_t
                     bidata = (int64_t*)duckdb_vector_get_data(colm);
-                    sprintf(buf, "%I64d\t", bidata[row]);
+                    sprintf(buf, "%I64d", bidata[row]);
                     printf(buf);
+                    strvec.push_back(buf);
                     break;
                 case DUCKDB_TYPE_DOUBLE:    // double
                     dbldata = (double*)duckdb_vector_get_data(colm);
-                    sprintf(buf, "%f\t", dbldata[row]);
+                    sprintf(buf, "%f", dbldata[row]);
                     printf(buf);
+                    strvec.push_back(buf);
                     break;
                 case DUCKDB_TYPE_VARCHAR:   // duckdb_string_t
                     // https://duckdb.org/docs/stable/clients/c/vector#strings
@@ -97,16 +104,10 @@ struct DuckDBFixture {
                         memcpy(buf, vcdata[row].value.inlined.inlined, vcdata[row].value.inlined.length);
                         buf[vcdata[row].value.inlined.length] = 0;
                         printf(buf);
-                        printf("\t");
-                        // yes, this fires the default ctor for
-                        // std::vector<std::string> on 1st visit
-                        std::vector<std::string>& strvec(varchars[col]);
                         strvec.push_back(buf);
                     }
                     else {
                         printf(vcdata[row].value.pointer.ptr);
-                        printf("\t");
-                        std::vector<std::string>& strvec(varchars[col]);
                         strvec.push_back(vcdata[row].value.pointer.ptr);
                     }
                     break;
@@ -131,10 +132,20 @@ struct DuckDBFixture {
                         decimal_value = (double)bidata[row] / decimal_divisor;
                         break;
                     case DUCKDB_TYPE_HUGEINT:   // duckdb_hugeint: 128
+                        hidata = (duckdb_hugeint*)duckdb_vector_get_data(colm);
+                        decimal_value = duckdb_hugeint_to_double(hidata[row]) / decimal_divisor;
                         break;
                     }
+                    // decimal scale 2 means we want "%.2f"
+                    // NB %% is the "escape sequence" for %
+                    // in a printf format, not \%.
+                    sprintf(fmtbuf, "%%.%df", decimal_scale);
+                    sprintf(buf, fmtbuf, decimal_value);
+                    printf(buf);
+                    strvec.push_back(buf);
                     break;
                 }
+                printf("\t");   // column separator for readability
                 buf[0] = 0;
             }
             printf("\n");
@@ -154,9 +165,10 @@ BOOST_FIXTURE_TEST_CASE(Varchar1, DuckDBFixture)
     auto rv = duckdb_query(con, "SELECT * FROM my_table;", &result);
     BOOST_TEST(rv == DuckDBSuccess);
     read_and_print_all_columns(result);
-    // [1] is a map key from colm index
-    BOOST_TEST(varchars[1].size() == 3);    // 3 non NULL in name col
-    BOOST_TEST(varchars[1] == expected_varchars);
+    // [1] is a map key from colm index: NB NULLs don't count as values
+    BOOST_TEST(values[0].size() == 4);    // 4 non NULL in id col
+    BOOST_TEST(values[1].size() == 3);    // 3 non NULL in name col
+    BOOST_TEST(values[1] == expected_varchars);
     // Destroy the result after use
     duckdb_destroy_result(&result);
 }
@@ -166,7 +178,9 @@ BOOST_FIXTURE_TEST_CASE(Summarize, DuckDBFixture)
     duckdb_result result;
     // sumarize varchar colms: name:0, type:1, min:2, max:3, avg:5, std:6, q25:7, q50:8, q75:9
     // other colms: apxu:4, cnt:10, null:11
-    std::vector<idx_t> expected_varchar_colms({0, 1, 2, 3, 5, 6, 7, 8, 9});
+    std::vector<idx_t> expected_colms({0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11});
+    std::vector<std::string> expected_null_counts({ "0.00","25.00" });  // col 11
+    std::vector<std::string> expected_counts({ "4","4" });              // col 10
 
     // Create a table and insert data
     duckdb_query(con, "CREATE TABLE my_table(id INTEGER, name VARCHAR);", NULL);
@@ -175,16 +189,21 @@ BOOST_FIXTURE_TEST_CASE(Summarize, DuckDBFixture)
     auto rv = duckdb_query(con, "summarize SELECT * FROM my_table;", &result);
     BOOST_TEST(rv == DuckDBSuccess);
     read_and_print_all_columns(result);
-    // [1] is a map key from colm index
-    auto evc_iter = expected_varchar_colms.begin();
-    for (; evc_iter != expected_varchar_colms.end(); ++evc_iter) {
+    // check we have 12 colms 0..11
+    auto evc_iter = expected_colms.begin();
+    for (; evc_iter != expected_colms.end(); ++evc_iter) {
         idx_t col_xpctd(*evc_iter);
-        auto map_iter = varchars.find(col_xpctd);
-        if (map_iter != varchars.end()) {
+        auto map_iter = values.find(col_xpctd);
+        if (map_iter != values.end()) {
             idx_t col_found = map_iter->first;
             BOOST_TEST(col_found == col_xpctd);
         }
     }
+
+    // column 11 is the NULL count as a 2 DP decimal
+    BOOST_TEST(values[11] == expected_null_counts);
+    // column 10 is count: should be 4 on both rows
+    BOOST_TEST(values[10] == expected_counts);
 
     // Destroy the result after use
     duckdb_destroy_result(&result);
