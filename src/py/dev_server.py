a60a88b947593627b835d1594ee9e33eee402478
John O'Sullivan <osullivj@gmail.com>
2025-11-17 16:43:22 +0000

    443 server for load_fonts.html to create NoDOM IndexedDB fonts in secure context

---
+++ src/py/dev_server.py
@@ -0,0 +1,44 @@
+# std pkgs
+import asyncio
+import logging
+import os.path
+# tornado
+import tornado
+import tornado.websocket
+from tornado.options import define, options, parse_command_line
+from tornado.web import StaticFileHandler
+# nodom
+import nd_consts
+import nd_web
+import nd_utils
+
+NDAPP='dev_server'
+
+logr = nd_utils.init_logging(NDAPP)
+
+EXTRA_HANDLERS = [
+    (r'/log_fonts.html', nd_web.HomeHandler),
+]
+
+# for security reasons duck only ingests parquet via 443
+define("port", default=443, help="run on the given port", type=int)
+
+service = nd_utils.Service(NDAPP, {}, {}, False)
+
+async def main():
+    parse_command_line()
+    cert_path = os.path.normpath(os.path.join(nd_consts.ND_ROOT_DIR, 'cfg'))
+    # fix the template path before the NDApp ctor fires
+    nd_web.web_args = ['src', 'web']
+    app = nd_web.NDApp(service, EXTRA_HANDLERS)
+    https_server = tornado.httpserver.HTTPServer(app, ssl_options={
+        "certfile": os.path.join(cert_path, "ssl_cert.pem"),
+        "keyfile": os.path.join(cert_path, "ssl_key.pem"),
+    })
+    https_server.listen(options.port)
+    logr.info(f'{NDAPP} port:{options.port} cert_path:{cert_path}')
+    await asyncio.Event().wait()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
