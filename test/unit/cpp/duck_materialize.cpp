f8e051352294e35179870fea5f2b9f204828b99b
John O'Sullivan <osullivj@gmail.com>
2025-12-08 15:15:08 +0000

    rwcnt undefined: test failing predictably

--- test/unit/cpp/duck_materialize.cpp
+++ test/unit/cpp/duck_materialize.cpp
@@ -1,10 +1,13 @@
 // Test harness for materializing chunks and
 // passing them to C++ WASM
+#include <string>
+#include <iostream>
 #include <emscripten/val.h>
 
 extern "C" {
     void on_db_result_cpp(emscripten::EM_VAL result_handle) {
         emscripten::val result = emscripten::val::take_ownership(result_handle);
-
+        auto row_count = result["rwcnt"].template as<int>();
+        std::cout << "rwcnt:" << row_count << std::endl;
     }
 };
