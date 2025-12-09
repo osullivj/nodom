5eef75ea2f0f431d4d6e7b67fb8cc3e122420bf7
John O'Sullivan <osullivj@gmail.com>
2025-12-09 17:13:54 +0000

    add get_chunk_cpp and on_chunk_cpp to separate chunk mem handling from DB event handling

--- test/unit/cpp/duck_materialize.cpp
+++ test/unit/cpp/duck_materialize.cpp
@@ -6,8 +6,33 @@
 
 extern "C" {
     void on_db_result_cpp(emscripten::EM_VAL result_handle) {
+        const static char* method = "on_db_result_cpp: ";
+    // void on_db_result_cpp(void* result_handle) {
         emscripten::val result = emscripten::val::take_ownership(result_handle);
-        auto row_count = result["rwcnt"].template as<int>();
-        std::cout << "rwcnt:" << row_count << std::endl;
+        std::string nd_type = result["nd_type"].template as<std::string>();
+        std::string query_id = result["query_id"].template as<std::string>();
+        // auto int_vec = emscripten::convertJSArrayToNumberVector<int>(result["types"]);
+        // auto str_vec = emscripten::vecFromJSArray<std::string>(result["names"]);
+        // auto row_count = result["rwcnt"].template as<int>();
+        std::cout << method << "nd_type: " << nd_type << ", qid: " << query_id << std::endl;
+    }
+
+    int get_chunk_cpp(uint8_t size) {
+        const static char* method = "get_chunk_cpp";
+        // batch_materializer calls us to get a handle on WASM memory
+        // https://stackoverflow.com/questions/56010390/emscripten-how-to-get-uint8-t-array-from-c-to-javascript
+        // Real impl will use some kind of mem pooling
+        uint8_t* buffer = new uint8_t(size);
+        int buffer_address = reinterpret_cast<int>(buffer);
+        printf("%s: buffer_address: %d\n", method, buffer_address);
+        return buffer_address;
+    }
+
+    void on_chunk_cpp(uint8_t chunk) {
+        const static char* method = "on_chunk_cpp";
+        uint8_t* chunk_ptr = &chunk;
+        int col_count = chunk_ptr[0];
+        int row_count = chunk_ptr[1];
+        printf("%s: col_count: %d, row_count: %d\n", method, col_count, row_count);
     }
 };
