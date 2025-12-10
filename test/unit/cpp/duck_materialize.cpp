56601297ff430d43742a1e23fbf1a4f19238c3c2
John O'Sullivan <osullivj@gmail.com>
2025-12-10 10:11:32 +0000

    Uint8Array data passed over in test for first time

--- test/unit/cpp/duck_materialize.cpp
+++ test/unit/cpp/duck_materialize.cpp
@@ -17,20 +17,21 @@ extern "C" {
         std::cout << method << "nd_type: " << nd_type << ", qid: " << query_id << std::endl;
     }
 
-    int get_chunk_cpp(uint8_t size) {
+    int get_chunk_cpp(int size) {
         const static char* method = "get_chunk_cpp";
         // batch_materializer calls us to get a handle on WASM memory
         // https://stackoverflow.com/questions/56010390/emscripten-how-to-get-uint8-t-array-from-c-to-javascript
         // Real impl will use some kind of mem pooling
-        uint8_t* buffer = new uint8_t(size);
+        uint8_t* buffer = new uint8_t[size];
+        memset(buffer, 0, size);
         int buffer_address = reinterpret_cast<int>(buffer);
-        printf("%s: buffer_address: %d\n", method, buffer_address);
+        printf("%s: size: %d, buffer_address: %d\n", method, size, buffer_address);
         return buffer_address;
     }
 
-    void on_chunk_cpp(uint8_t chunk) {
+    void on_chunk_cpp(int* chunk) {
         const static char* method = "on_chunk_cpp";
-        uint8_t* chunk_ptr = &chunk;
+        uint8_t* chunk_ptr = reinterpret_cast<uint8_t*>(chunk);
         int col_count = chunk_ptr[0];
         int row_count = chunk_ptr[1];
         printf("%s: col_count: %d, row_count: %d\n", method, col_count, row_count);
