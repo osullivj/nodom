633d1aafc17149986d5d1bb6aaadc42f824f2136
John O'Sullivan <osullivj@gmail.com>
2025-12-10 12:39:54 +0000

    switch test to use HEAPU32

--- test/unit/cpp/duck_materialize.cpp
+++ test/unit/cpp/duck_materialize.cpp
@@ -27,8 +27,8 @@ extern "C" {
         // batch_materializer calls us to get a handle on WASM memory
         // https://stackoverflow.com/questions/56010390/emscripten-how-to-get-uint8-t-array-from-c-to-javascript
         // Real impl will use some kind of mem pooling
-        uint16_t* buffer = new uint16_t[size];
-        memset(buffer, 0, size*2);
+        uint32_t* buffer = new uint32_t[size];
+        memset(buffer, 0, size*4);
         int buffer_address = reinterpret_cast<int>(buffer);
         printf("%s: size: %d, buffer_address: %d\n", method, size, buffer_address);
         return buffer_address;
@@ -36,23 +36,27 @@ extern "C" {
 
     void on_chunk_cpp(int* chunk) {
         const static char* method = "on_chunk_cpp";
-        uint16_t* chunk_ptr = reinterpret_cast<uint16_t*>(chunk);
-        // First 3 bytes are done, col_count, row_count
-        int buffer_position = 0;
-        int done = chunk_ptr[buffer_position++];
-        int col_count = chunk_ptr[buffer_position++];
-        int row_count = chunk_ptr[buffer_position++];
+        uint32_t* chunk_ptr = reinterpret_cast<uint32_t*>(chunk);
+        // First 3 words are done, col_count, row_count
+        int bptr = 0;
+        int done = chunk_ptr[bptr++];
+        int col_count = chunk_ptr[bptr++];
+        int row_count = chunk_ptr[bptr++];
         printf("%s: done: %d, col_count: %d, row_count: %d\n", method, done, col_count, row_count);
         // next col_count bytes are the types
         for (int i = 0; i < col_count; i++) {
-            int tipe = chunk_ptr[buffer_position++];
+            int tipe = chunk_ptr[bptr++];
             printf("%s: type: %d\n", method, tipe);
         }
         // next col_count zero terminated ASCII strings
         for (int i = 0; i < col_count; i++) {
-            char* name = reinterpret_cast<char*>(chunk_ptr[buffer_position]);
-            printf("%s: name: %s\n", method, name);
-            buffer_position += 1 + strlen(name);
+            std::string name;
+            while (chunk[bptr] != 0) {
+                name.push_back(chunk[bptr]);
+                bptr++;
+            }
+            printf("%s: name: %s\n", method, name.c_str());
+            bptr++;
         }
     }
 };
