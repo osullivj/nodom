9849b869f78acd92350abf4be19f943362c7226b
John O'Sullivan <osullivj@gmail.com>
2025-12-22 16:53:03 +0000

    helpers in nd_types.hpp now

--- test/unit/cpp/duck_materialize.cpp
+++ test/unit/cpp/duck_materialize.cpp
@@ -3,45 +3,8 @@
 #include <string>
 #include <iostream>
 #include <emscripten/val.h>
+#include "nd_types.hpp"
 
-// types = 2, 10, 3, 2, 3, 3, 2, 2, 10, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 2, 10, 10
-// typ_o = Int32, Timestamp<MICROSECOND>, Float64, Int32, Float64, Float64, Int32, Int32, Timestamp<MICROSECOND>, Int32, Int32, Int32, Int32, Int32, Int32, Int32, Int32, Int32, Int32, Float64, Float64, Float64, Float64, Float64, Float64, Float64, Float64, Float64, Float64, Int32, Timestamp<MICROSECOND>, Timestamp<MICROSECOND>
-// batch_materializer JS code stores type as uint32
-// ND these enums are not the same as duckdb.h!
-enum DuckType : uint32_t {
-    Int32 = 2,
-    Timestamp_micro = 10,
-    Float64 = 3,
-    Utf8 = 5
-};
-
-const char* DuckTypeToString(DuckType dt) {
-    switch (dt) {
-    case DuckType::Int32:
-        return "Int32";
-    case DuckType::Float64:
-        return "Float64";
-    case DuckType::Timestamp_micro:
-        return "Timestamp_micro";
-    case DuckType::Utf8:
-        return "Utf8";
-    }
-    return "Unknown";
-}
-
-int DuckTypeToSize(DuckType dt) {
-    switch (dt) {
-    case DuckType::Int32:
-        return 4;
-    case DuckType::Float64:
-        return 8;
-    case DuckType::Timestamp_micro:
-        return 4;
-    case DuckType::Utf8:
-        return 0;
-    }
-    return -1;
-}
 
 void printf_comma(int i, int col_count) {
     if (i == col_count - 1) printf("\n");
