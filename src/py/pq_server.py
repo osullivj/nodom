b0acf40e46a30508995ee90dbe28f50159fda838
John O'Sullivan <osullivj@gmail.com>
2026-01-08 15:49:58 +0000

    tidy

--- src/py/pq_server.py
+++ src/py/pq_server.py
@@ -1,23 +1,27 @@
 # std pkgs
 import asyncio
-import logging
 import os.path
+
+# nodom
+import nd_consts
+import nd_utils
+import nd_web
+
 # tornado
 import tornado
 import tornado.websocket
 from tornado.options import define, options, parse_command_line
-from tornado.web import StaticFileHandler
-# nodom
-import nd_consts
-import nd_web
-import nd_utils
 
-NDAPP='pq_server'
+NDAPP = "pq_server"
 
 logr = nd_utils.init_logging(NDAPP)
 
 EXTRA_HANDLERS = [
-    (r'/api/parquet/(.*)', nd_web.ParquetHandler, dict(path=os.path.join(nd_consts.ND_ROOT_DIR, 'dat')))
+    (
+        r"/api/parquet/(.*)",
+        nd_web.ParquetHandler,
+        dict(path=os.path.join(nd_consts.ND_ROOT_DIR, "dat")),
+    )
 ]
 
 # for security reasons duck only ingests parquet via 443
@@ -25,16 +29,20 @@ define("port", default=443, help="run on the given port", type=int)
 
 service = nd_utils.Service(NDAPP, {}, {}, False)
 
+
 async def main():
     parse_command_line()
-    cert_path = os.path.normpath(os.path.join(nd_consts.ND_ROOT_DIR, 'cfg'))
+    cert_path = os.path.normpath(os.path.join(nd_consts.ND_ROOT_DIR, "cfg"))
     app = nd_web.NDApp(service, EXTRA_HANDLERS)
-    https_server = tornado.httpserver.HTTPServer(app, ssl_options={
-        "certfile": os.path.join(cert_path, "ssl_cert.pem"),
-        "keyfile": os.path.join(cert_path, "ssl_key.pem"),
-    })
+    https_server = tornado.httpserver.HTTPServer(
+        app,
+        ssl_options={
+            "certfile": os.path.join(cert_path, "ssl_cert.pem"),
+            "keyfile": os.path.join(cert_path, "ssl_key.pem"),
+        },
+    )
     https_server.listen(options.port)
-    logr.info(f'{NDAPP} port:{options.port} cert_path:{cert_path}')
+    logr.info(f"{NDAPP} port:{options.port} cert_path:{cert_path}")
     await asyncio.Event().wait()
 
 
