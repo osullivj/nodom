77f2e6c457f4a34872a5b079d5257896192faf85
John O'Sullivan <osullivj@gmail.com>
2026-01-15 16:17:57 +0000

    zed cmd line launch

--- sh/ndide.bat
+++ sh/ndide.bat
@@ -1,7 +1,8 @@
 @echo off
 echo "'start devenv' to launch the MSVC IDE"
+echo "'start zed' to launch the Zed editor"
 :: IDE env vars
 set PATH=C:\osullivj\src\h3gui\venv\Lib\site-packages\pyarrow;%PATH%
 "%ND_MSVC_HOME%\VC\Auxiliary\Build\vcvars64.bat"
 :: user: "start devenv"
-
+:: user: "start zed"
