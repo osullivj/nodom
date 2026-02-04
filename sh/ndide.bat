ee66424c5fa509bd6884b4383308f4dfc45fbcbc
John O'Sullivan <osullivj@gmail.com>
2026-02-04 17:07:50 +0000

    MS PIX cfg

--- sh/ndide.bat
+++ sh/ndide.bat
@@ -1,8 +1,10 @@
 @echo off
 echo "'start devenv' to launch the MSVC IDE"
 echo "'start zed' to launch the Zed editor"
+echo "'start winpix' to launch the MS PIX profiler"
 :: IDE env vars
 set PATH=C:\osullivj\src\h3gui\venv\Lib\site-packages\pyarrow;%PATH%
 "%ND_MSVC_HOME%\VC\Auxiliary\Build\vcvars64.bat"
 :: user: "start devenv"
 :: user: "start zed"
+:: user: "start winpix"
