d363f5183f85bff2e6e1e5730c40f911fbef3640
John O'Sullivan <osullivj@gmail.com>
2026-02-07 15:46:38 +0000

    adding more pix report stuff; need to refactor especially as we'll do Chrome reporting too

--- src/cpp/pix.cpp
+++ src/cpp/pix.cpp
@@ -4,6 +4,49 @@
 #include "pix3.h"
 #endif
 
+#include "nd_types.hpp"
+
+// TODO: refactor pix.cpp for Chrome and 
+// gen util funcs
+const char* DuckTypeToString(DuckType dt) {
+    switch (dt) {
+    case DuckType::Int:
+        return "Int";
+    case DuckType::Float:
+        return "Float";
+    case DuckType::Timestamp:
+        return "TS";
+    case DuckType::Timestamp_s:
+        return "TS_s";
+    case DuckType::Timestamp_ms:
+        return "TS_ms";
+    case DuckType::Timestamp_us:
+        return "TS_us";
+    case DuckType::Timestamp_ns:
+        return "TS_ns";
+    case DuckType::Utf8:
+        return "Utf8";
+    }
+    return "Unknown";
+}
+
+int DuckTypeToSize(DuckType dt) {
+    switch (dt) {
+    case DuckType::Int:
+        return 4;
+    case DuckType::Float:
+    case DuckType::Timestamp:
+    case DuckType::Timestamp_s:
+    case DuckType::Timestamp_ms:
+    case DuckType::Timestamp_us:
+    case DuckType::Timestamp_ns:
+    case DuckType::Utf8:
+        return 8;
+    }
+    return 0;
+}
+
+
 #ifdef USE_PIX
 uint32_t render_color = PIX_COLOR(255, 0, 125);
 uint32_t dbase_color = PIX_COLOR(125, 0, 255);
@@ -51,5 +94,29 @@ void pix_begin_dbase() {
 void pix_end_event() {
 #ifdef USE_PIX
 	PIXEndEvent();
+#endif
+}
+
+const wchar_t* PixReportTypeToString(PixReportType rt) {
+    switch (rt) {
+    case RenderFPS:
+        return L"RenderFPS";
+    case RenderPushPC:
+        return L"RenderPushPC";
+    case RenderPopPC:
+        return L"RenderPopPC";
+    case DBScans:
+        return L"DBScans";
+    case DBQueries:
+        return L"DBQueries";
+    case DBBatches:
+        return L"DBBatches";
+    }
+    return L"Unknown";
+}
+
+void pix_report(PixReportType t, float val) {
+#ifdef USE_PIX
+	PIXReportCounter(PixReportTypeToString(t), val);
 #endif
 }
