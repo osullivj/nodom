3b2582a5cfd9bb2fbdae59e4e214350572f1c5aa
John O'Sullivan <osullivj@gmail.com>
2026-03-13 15:21:21 +0000

    AddInt test case passes

--- test/unit/cpp/data_cache.cpp
+++ test/unit/cpp/data_cache.cpp
@@ -5,7 +5,7 @@
 
 struct DataCacheFixture {
     DataCache dc;
-
+    int style_coloring{ StyleColor::Dark };
     DataCacheFixture() {
     }
 
@@ -16,10 +16,21 @@ struct DataCacheFixture {
 BOOST_FIXTURE_TEST_CASE(AddAddr, DataCacheFixture)
 {
     std::string addr_str{ "integer_address" };
-
     AInx addr_inx = dc.add_address(std::string{"integer_address"});
+
     BOOST_TEST(AInx::item_type == CIT::Address);
     BOOST_TEST(AInx::data_type == CDT::cdStr);
     BOOST_TEST(addr_inx.magic_index == 0x00140000);
     BOOST_TEST(addr_inx() == 0);
+}
+
+BOOST_FIXTURE_TEST_CASE(AddInt, DataCacheFixture)
+{
+    std::string addr_str{ "integer_address" };
+    AInx addr_inx = dc.add_address(std::string{ "integer_address" });
+    IntInx int_inx = dc.add_int(&style_coloring);
+    BOOST_TEST(IntInx::item_type == CIT::Value);
+    BOOST_TEST(IntInx::data_type == CDT::cdInt);
+    BOOST_TEST(int_inx.magic_index == 0x00210000);
+    BOOST_TEST(int_inx() == 0);
 }
