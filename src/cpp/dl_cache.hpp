b9939eaef1aa705da65bb87102037bb1aa779380
John O'Sullivan <osullivj@gmail.com>
2026-03-18 12:24:06 +0000

    cspec name str internalisation

--- src/cpp/dl_cache.hpp
+++ src/cpp/dl_cache.hpp
@@ -25,6 +25,43 @@ private:
     // Cache addresses
     std::set<AddrInx>   addr_set;
 
+    // interned str for eg cname, title, title_font
+    std::map<CacheSpecifier, StrInx> cspec_indices;
+
+    inline static std::array<const char*, cs_end_cache_specs> atomic_cspec_names{
+        Static::title_cs,      // cs_title
+        Static::title_font_cs,      // cs_title_font
+        Static::title_font_size_cs,      // cs_title_font_size
+        Static::body_font_cs,      // cs_body_font
+        Static::body_font_size_cs,      // cs_body_font_size
+        Static::button_font_cs,      // cs_button_font
+        Static::button_font_size_cs,      // cs_button_font_size
+        Static::year_month_font_cs,      // cs_year_month_font
+        Static::year_month_font_size_cs,      // cs_year_month_font_size
+        Static::day_date_font_cs,      // cs_day_date_font
+        Static::day_date_font_size_cs,      // cs_day_date_font_size
+        Static::font_cs,                    // cs_font
+        Static::font_size_cs,               // cs_font_size
+        Static::label_cs,      // cs_label
+        Static::text_cs,      // cs_text
+        Static::step_cs,      // cs_step,
+        Static::step_fast_cs,      // cs_step_fast
+        Static::spinner_radius_cs,      // cs_spinner_radius
+        Static::spinner_thickness_cs,      // cs_spinner_thickness
+        Static::flags_cs,      // cs_flags
+        Static::table_flags_cs,      // cs_table_flags
+        Static::combo_flags_cs,      // cs_combo_flags
+        Static::window_flags_cs,      // cs_window_flags
+        Static::db_cs,     // cs_db,
+        Static::fps_cs,     // cs_fps,
+        Static::demo_cs,     // cs_demo,
+        Static::id_stack_cs,     // cs_id_stack,
+        Static::font_scale_cs,    // cs_font_scale,
+        Static::style_cs,        // cs_style
+        Static::cname_cs,
+        Static::index_cs,
+        Static::qname_cs
+    };
 protected:
     template <CIT itype>
     auto intern_string(std::string&& s) {
@@ -52,6 +89,17 @@ protected:
 
 public:
     DataLayCache() { }
+
+    void init() {
+        const static char* method = "DataLayCache::init: ";
+
+        // intern the cache spec names so each has a str val DCI
+        for (uint32_t spec_inx{ cs_title }; spec_inx < cs_end_cache_specs; ++spec_inx) {
+            StrInx sinx = intern_string<Value>(atomic_cspec_names[spec_inx]);
+            NDLogger::cout() << method << atomic_cspec_names[spec_inx] << ":" << sinx << std::endl;
+            cspec_indices.emplace(std::make_pair<CacheSpecifier, StrInx>(CacheSpecifier{ spec_inx }, std::move(sinx)));
+        }
+    }
 
     size_t addr_set_size() { return addr_set.size(); }
 
