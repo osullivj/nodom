da9dea77e2f04066d4d0cbb135600f0334faa815
John O'Sullivan <osullivj@gmail.com>
2026-05-07 16:00:42 +0100

    main for ndcc

---
+++ src/cpp/main_ndcc.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "static_strings.hpp"
+#include "dl_cache.hpp"
+
+// NDCC: NoDOM compiler
+
+template <typename JSON>
+struct DLC : public DataLayCache<JSON> {
+    void on_init() { }
+};
+
+int main(int argc, char* argv[]) {
+    if (argc < 3) {
+        std::cout << "Usage" << std::endl;
+        std::cout << "ndcc.exe <jsonDataPath> <jsonLayoutPath>" << std::endl;
+        return 1;
+    }
+
+    DLC<nlohmann::json> data_lay_cache;
+    try {
+        std::string init_data(load_json(argv[1]));
+        std::string init_layout(load_json(argv[2]));
+        auto data = JParse<nlohmann::json>(init_data);
+        auto layout = JParse<nlohmann::json>(init_layout);
+
+        data_lay_cache.on_json(data, layout, [&]() {data_lay_cache.on_init(); });
+
+        data_lay_cache.report_sanity_check();
+        int bsc = data_lay_cache.report_cache_strings();
+        int bic = data_lay_cache.report_cache_ints();
+        int bfc = data_lay_cache.report_cache_floats();
+        data_lay_cache.report_address_map();
+        data_lay_cache.report_actions();
+    }
+    catch (nlohmann::json::exception& ex) {
+        std::cout << ex.what() << std::endl;
+    }
+}
