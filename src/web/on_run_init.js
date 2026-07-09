056c1e1e01b51935f959da09588d0f520ff262a4
John O'Sullivan <osullivj@gmail.com>
2026-07-09 16:57:54 +0100

    --pre-js LDFLAG impl to install EMS C++ event handlers

---
+++ src/web/on_run_init.js
@@ -0,0 +1,14 @@
+Module["onRuntimeInitialized"] = function () {
+  if (typeof Module !== "undefined") {
+    let on_db_result_cpp = Module.cwrap("on_db_result_cpp", "void", ["object"]);
+    let get_chunk_cpp = Module.cwrap("get_chunk_cpp", "number", [
+      "string",
+      "number",
+    ]);
+    let on_chunk_cpp = Module.cwrap("on_chunk_cpp", "void", ["number"]);
+    on_db_result = function (result_object) {
+      let result_handle = Emval.toHandle(result_object);
+      on_db_result_cpp(result_handle);
+    };
+  }
+};
