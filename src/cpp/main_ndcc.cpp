f936329a12f5b8da0eccdb129f36c168720607a1
John O'Sullivan <osullivj@gmail.com>
2026-07-15 16:34:47 +0100

    report menus, funcs and data_refs

--- src/cpp/main_ndcc.cpp
+++ src/cpp/main_ndcc.cpp
@@ -31,7 +31,11 @@ int main(int argc, char* argv[]) {
         int bic = data_lay_cache.report_cache_ints(eic);
         int bfc = data_lay_cache.report_cache_floats(efc);
         data_lay_cache.report_address_map();
+        data_lay_cache.report_menu_entity_map();
+        data_lay_cache.report_data_refs();
+        data_lay_cache.report_func_maps();
         data_lay_cache.report_actions();
+        data_lay_cache.report_cache_errors();
     }
     catch (nlohmann::json::exception& ex) {
         std::cout << ex.what() << std::endl;
