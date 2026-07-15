74ac1aff53a922af30259e64033c74c0b11d0ae2
John O'Sullivan <osullivj@gmail.com>
2026-07-15 12:07:28 +0100

    add two async funcs: FDec1, FDec2

--- src/web/incdec.js
+++ src/web/incdec.js
@@ -1,21 +1,65 @@
 function FInc1(data) {
   data["op1"] += 1;
-  return {
+  var rv = {
     nd_type: "DataChange",
     cache_key: "op1",
     new_value: data["op1"],
   };
+  console.log("FInc1: " + JSON.stringify(rv));
+  return rv;
 }
 
 function FInc2(data) {
   data["op1"] += 2;
-  return {
+  var rv = {
     nd_type: "DataChange",
     cache_key: "op1",
     new_value: data["op1"],
   };
+  console.log("FInc2: " + JSON.stringify(rv));
+  return rv;
 }
 
-window["__nodom__"] = {
-  functions: [FInc1, FInc2]
-};
+async function FDec1(request) {
+  var url = "https://localhost/api/fdec1/" + request.data["op1"];
+  console.log("FDec1: awaiting " + url);
+  var resp = await fetch(url);
+  console.log("FDec1: response " + resp);
+  if (resp.ok) {
+    data["op1"] = await resp.json();
+    return {
+      nd_type: "FunctionResult",
+      query_id: nd_db_request.query_id,
+      cache_key: "op1",
+      new_value: data["op1"],
+    };
+    return {
+      nd_type: "FunctionResult",
+      query_id: nd_db_request.query_id,
+      error: "fetch"
+    };
+  }
+}
+
+async function FDec2(request) {
+  var url = "https://localhost/api/fdec2/" + request.data["op1"];
+  console.log("FDec2: awaiting " + url);
+  var resp = await fetch(url);
+  console.log("FDec2: response " + resp);
+  if (resp.ok) {
+    data["op1"] = await resp.json();
+    return {
+      nd_type: "FunctionResult",
+      query_id: nd_db_request.query_id,
+      cache_key: "op1",
+      new_value: data["op1"],
+    };
+    return {
+      nd_type: "FunctionResult",
+      query_id: nd_db_request.query_id,
+      error: "fetch"
+    };
+  }
+}
+
+Module["nodom_functions"] = [FInc1, FInc2, FDec1, FDec2];
