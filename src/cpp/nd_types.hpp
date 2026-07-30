30166f7e423ccaf0b425f52c1d27c7412441fe46
John O'Sullivan <osullivj@gmail.com>
2026-07-30 11:17:07 +0100

    add tooltip

--- src/cpp/nd_types.hpp
+++ src/cpp/nd_types.hpp
@@ -451,6 +451,7 @@ enum CacheSpecifier : uint32_t {
     cs_window_flags,
     cs_column_flags,
     cs_close_button,
+    cs_tooltip,
     cs_show_lines,
     cs_show_fills,
     cs_shaded_plot_flags,
