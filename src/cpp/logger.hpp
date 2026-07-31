48fc3b603ebd1e046d3ec3c4181b0cec9d89e67a
John O'Sullivan <osullivj@gmail.com>
2026-07-31 12:29:11 +0100

    make logging to imgui DebugLog configgable

--- src/cpp/logger.hpp
+++ src/cpp/logger.hpp
@@ -19,6 +19,18 @@ static boost::mutex log_mutex;
 #endif
 
 class NDOutBuffer : public std::stringbuf {
+private:
+	std::string format;	// "NDI:%s" or "NDE:%s"
+	bool		imgui_log{ false };
+public:
+	virtual ~NDOutBuffer() {}
+
+	NDOutBuffer(const std::string& fmt, bool imlog=false)
+		:format(fmt), imgui_log(imlog), std::stringbuf() { }
+
+	void set_imgui_logging(bool imlog) {
+		imgui_log = imlog;
+	}
 protected:
 	// https://en.cppreference.com/w/cpp/io/basic_streambuf/pubsync.html
 	virtual int sync() {
@@ -27,24 +39,10 @@ protected:
 		std::cout << this->str();
 #else
 		emscripten_log(EM_LOG_CONSOLE | EM_LOG_INFO, "%s", this->str().c_str());
-		ImGui::DebugLog("NDI:%s", this->str().c_str());
 #endif
-		// clear buf by setting contents to empty string
-		this->str("");
-		return 0;	// success
-	}
-};
-
-class NDErrBuffer : public std::stringbuf {
-protected:
-	virtual int sync() {
-#ifndef __EMSCRIPTEN__
-		boost::unique_lock<boost::mutex> log_lock(log_mutex);
-		std::cerr << this->str();
-#else
-		emscripten_log(EM_LOG_CONSOLE | EM_LOG_ERROR, "%s", this->str().c_str());
-		ImGui::DebugLog("NDE:%s", this->str().c_str());
-#endif
+		if (imgui_log) {
+			ImGui::DebugLog(format.c_str(), this->str().c_str());
+		}
 		// clear buf by setting contents to empty string
 		this->str("");
 		return 0;	// success
@@ -57,26 +55,31 @@ protected:
 // https://stackoverflow.com/questions/13703823/a-custom-ostream
 class NDLogger {
 private:
-	NDOutBuffer		out_buffer;
-	NDErrBuffer		err_buffer;
-	std::ostream	out_stream{ &out_buffer };
+	NDOutBuffer		info_buffer{Static::info_format_cs};
+	NDOutBuffer		err_buffer{Static::err_format_cs};
+	std::ostream	info_stream{ &info_buffer };
 	std::ostream	err_stream{ &err_buffer };
 
 public:
-	std::ostream& out() { return out_stream; }
+	std::ostream& out() { return info_stream; }
 	std::ostream& err() { return err_stream; }
 
-	static NDLogger& getInstance() {
+	static NDLogger& get_instance() {
 		// using the Scott Meyers singleton pattern
 		static NDLogger instance;
 		return instance;
 	}
 
 	static std::ostream& cout() {
-		return NDLogger::getInstance().out();
+		return NDLogger::get_instance().out();
 	}
 
 	static std::ostream& cerr() {
-		return NDLogger::getInstance().err();
+		return NDLogger::get_instance().err();
+	}
+
+	void set_imgui_logging(bool imlog) {
+		info_buffer.set_imgui_logging(imlog);
+		err_buffer.set_imgui_logging(imlog);
 	}
 };
