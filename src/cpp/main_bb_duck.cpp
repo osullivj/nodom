bd7a172ad19a4bc7870213f43f4ad02d28b51677
John O'Sullivan <osullivj@gmail.com>
2026-09-02 15:49:15 +0100

    event refactor: rename server to bulk

--- src/cpp/main_bb_duck.cpp
+++ src/cpp/main_bb_duck.cpp
@@ -101,18 +101,18 @@ int main(int argc, char* argv[]) {
         exit(1);
     }
 
-    DuckDB_t server;
-    NDContext<json_t, DuckDB_t> ctx(server, app_key, ini_path,
+    DuckDB_t bulk;
+    NDContext<json_t, DuckDB_t> ctx(bulk, app_key, ini_path,
             init_data.empty() ? nullptr : init_data.c_str(), 
             init_layout.empty() ? nullptr : init_layout.c_str());
 
     try {
         // launch DB thread: see db_loop impls
-        server.start_db_thread();
+        bulk.start_db_thread();
         // now launch websock client with a boost::asio
         // event loop on the main thread to dispatch
         // the timeout and on_message callbacks
-        NDWebSockClient<json_t, DuckDB_t> ws_client(server, ctx);
+        NDWebSockClient<json_t, DuckDB_t> ws_client(bulk, ctx);
         ws_client.run();
     }
     catch (websocketpp::exception const& ex) {
