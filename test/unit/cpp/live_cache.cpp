3c39cad3fd239133e54e92c63963ee93b05670d8
John O'Sullivan <osullivj@gmail.com>
2026-09-04 12:16:13 +0100

    UpdateOneTicker test passes

--- test/unit/cpp/live_cache.cpp
+++ test/unit/cpp/live_cache.cpp
@@ -14,24 +14,34 @@ using json_t = nlohmann::json;
 using Tiingo_t = TiingoIEXMidRecords<json_t>;
 using MktData_t = LiveCache<json_t, Tiingo_t>;
 
+using dbl_nanosecs = std::chrono::duration<double, std::nano>;
+
+
 struct LiveCacheFixture {
     MktData_t       live;
     nlohmann::json  update;
-    nlohmann::json  response;
+    nlohmann::json  sub_response;
     StringVec       ticker_list;
 
     LiveCacheFixture() {
         update[Static::nd_type_cs] = Static::live_update_cs;
+        sub_response[Static::nd_type_cs] = Static::live_response_cs;
     }
     ~LiveCacheFixture() { }
+
+    double now_as_double() {
+        auto now = std::chrono::system_clock::now();
+        auto duration_ns = std::chrono::duration_cast<dbl_nanosecs>(now.time_since_epoch());
+        return duration_ns.count();
+    }
 };
 
 BOOST_FIXTURE_TEST_CASE(SubToOneTicker, LiveCacheFixture)
 {
     live.init(2);
     ticker_list.push_back("spy");
-    update[Static::tickers_cs] = JArray(ticker_list);
-    live.on_sub(update);
+    sub_response[Static::tickers_cs] = JArray(ticker_list);
+    live.on_sub(sub_response);
     BOOST_TEST(live.ticker_count() == 1);
 }
 
@@ -40,8 +50,8 @@ BOOST_FIXTURE_TEST_CASE(SubToTwoTicker, LiveCacheFixture)
     live.init(2);
     ticker_list.push_back("spy");
     ticker_list.push_back("vym");
-    update[Static::tickers_cs] = JArray(ticker_list);
-    live.on_sub(update);
+    sub_response[Static::tickers_cs] = JArray(ticker_list);
+    live.on_sub(sub_response);
     BOOST_TEST(live.ticker_count() == 2);
 }
 
@@ -51,7 +61,23 @@ BOOST_FIXTURE_TEST_CASE(SubToThreeTicker, LiveCacheFixture)
     ticker_list.push_back("spy");
     ticker_list.push_back("vym");
     ticker_list.push_back("dell");
-    update[Static::tickers_cs] = JArray(ticker_list);
-    live.on_sub(update);
+    sub_response[Static::tickers_cs] = JArray(ticker_list);
+    live.on_sub(sub_response);
     BOOST_TEST(live.ticker_count() == 2);
+}
+
+BOOST_FIXTURE_TEST_CASE(UpdateOneTicker, LiveCacheFixture)
+{
+    live.init(2);
+    ticker_list.push_back("dell");
+    sub_response[Static::tickers_cs] = JArray(ticker_list);
+    live.on_sub(sub_response);
+    update[Static::ticker_cs] = "dell";
+    double mid{ 103.25 };
+    double ts{ now_as_double() };
+    update[Static::mid_cs] = mid;
+    update[Static::timestamp_cs] = ts;
+    live.on_update(update);
+    BOOST_TEST(live.records.mid[0] == mid);
+    BOOST_TEST(live.records.time_stamp[0] == ts);
 }
