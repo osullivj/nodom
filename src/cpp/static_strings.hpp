301c95dc5faf0c9d1c4394301a863b59f3d8fe72
John O'Sullivan <osullivj@gmail.com>
2026-09-17 12:02:44 +0100

    NDF store/fetch ops

--- src/cpp/static_strings.hpp
+++ src/cpp/static_strings.hpp
@@ -203,6 +203,8 @@ struct Static {
 
 	// NDF (NoDOM Forth) operands
 	inline static const char* ndfop_index_cs{ "[]" };
+	inline static const char  ndfop_store_c{ '>' };
+	inline static const char  ndfop_fetch_cs{ '<' };
 
 	inline static const char* gui_cs{ "GUI" };
 	inline static const char* websock_cs{ "WebSock" };
