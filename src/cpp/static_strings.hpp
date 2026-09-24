6df70ab57d589dc4ea4b63a06820594cd4394e4e
John O'Sullivan <osullivj@gmail.com>
2026-09-24 11:15:29 +0100

    RenderTable->RenderBulkTable, render_table()->render_bulk_table()

--- src/cpp/static_strings.hpp
+++ src/cpp/static_strings.hpp
@@ -17,7 +17,7 @@ struct Static {
 	inline static const char* rm_checkbox_cs{ "Checkbox" };
 	inline static const char* rm_text_cs{ "Text" };
 	inline static const char* rm_button_cs{ "Button" };
-	inline static const char* rm_table_cs{ "Table" };
+	inline static const char* rm_bulk_table_cs{ "BulkTable" };
 	inline static const char* rm_live_table_cs{ "LiveTable" };
 	inline static const char* rm_footer_cs{ "Footer" };		// Compount
 	inline static const char* rm_debug_footer_cs{ "DebugFooter" };		// Compount
